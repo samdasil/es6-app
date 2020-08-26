13c0dd41545ecc65c4700b0979863b433cddc925
Sammy Silva <silvasammy@outlook.com>
2020-08-26 15:37:14 -0400

    desafio 1 - exercicio 2.1,2.2,2.3,2.4

--- desafio_1/exercicio_2.js
+++ desafio_1/exercicio_2.js
@@ -4,7 +4,10 @@ const usuarios = [
     { nome: 'Lucas', idade: 30, empresa: 'Facebook' }
 ];
 
+//exercicio 2.1
+
 // normal function
+/* 
 const idade1 = usuarios.map(function({idade}){
     return idade;
 });
@@ -12,7 +15,49 @@ const idade1 = usuarios.map(function({idade}){
 // arrow function
 const idade2 = usuarios.map( ({idade})  =>  idade);
 
-
 console.log(idade1);
+console.log(idade2); 
+*/
 
-console.log(idade2);
+
+// exercicio 2.2
+
+// normal function
+/* 
+const workRocket1 = usuarios.filter(function(usuarios){
+    return (usuarios.idade > 18 && usuarios.empresa === 'Rocketseat');
+})
+
+// arrow function
+const workRocket2 = usuarios.filter( usuarios => usuarios.idade > 18 && usuarios.empresa === 'Rocketseat');
+
+console.log(workRocket1);
+console.log(workRocket2); 
+*/
+
+// exercicio 2.3
+
+// normal function
+/* 
+const workRocket1 = usuarios.find(function(usuarios){
+    return (usuarios.empresa  === 'Google');
+})
+
+// arrow function
+const workRocket2 = usuarios.find( usuarios => usuarios.empresa === 'Google');
+
+console.log(workRocket1);
+console.log(workRocket2);
+*/
+
+// exercicio 2.4
+
+// normal function
+const workRocket1 = usuarios.map(function(usuarios){
+    usuarios.idade *= 2;
+    return usuarios;
+}).filter(function(usuarios){
+    return usuarios.idade <= 50;
+});
+
+console.log(workRocket1);
--- desafio_1/exercicio_3.js
+++ desafio_1/exercicio_3.js
@@ -3,14 +3,3 @@ const usuarios = [
     { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
     { nome: 'Lucas', idade: 30, empresa: 'Facebook' }
 ];
-
-// normal function
-const workRocket1 = usuarios.filter(function(usuarios){
-    return (usuarios.idade > 18 && usuarios.empresa === 'Rocketseat');
-})
-
-// arrow function
-const workRocket2 = usuarios.filter( usuarios => usuarios.idade > 18 && usuarios.empresa === 'Rocketseat');
-
-console.log(workRocket1);
-console.log(workRocket2);
